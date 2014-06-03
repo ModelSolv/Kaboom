9c6bc49096c8466639b08b567c3003db47e22123
Ted Epstein <ted.epstein@modelsolv.com>
2014-06-03 17:13:21 -0400

    Fixed call to getCanonicalObject()

--- src/main/java/com/modelsolv/kaboom/serializer/AbstractSerializerImpl.java
+++ src/main/java/com/modelsolv/kaboom/serializer/AbstractSerializerImpl.java
@@ -8,8 +8,8 @@ public abstract class AbstractSerializerImpl implements Serializer {
 	
 	 @Override
 	public String serialize(Resource res, CanonicalObjectReader reader) {
-			// The object bound to this resource.  A POJO wrapped to provide easey property access.
-			CanonicalObjectReader obj = res.getBoundObject();
+			// The object bound to this resource.  .
+			Object obj = res.getCanonicalObject();
 			// The resource data model, with property exclusions and reference treatments.
 			ResourceDataModel rdm = res.getResourceDataModel();
 			return serialize(obj, reader, rdm);
